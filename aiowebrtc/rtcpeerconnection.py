b9ccace40f3c676b4ac3c4d4c09776656653eabd
Jeremy Lainé <jeremy.laine@m4x.org>
2018-02-24 15:06:37 +0100

    [rtcpeerconnection] add bogus audio codec for now

--- aiowebrtc/rtcpeerconnection.py
+++ aiowebrtc/rtcpeerconnection.py
@@ -101,9 +101,14 @@ class RTCPeerConnection:
             't=0 0',
         ]
 
+        default_candidate = self.__iceConnection.get_default_candidate(1)
         sdp += [
-            'c=IN IP4 0.0.0.0',
+            # FIXME: negotiate codec
+            'm=audio %d UDP/TLS/RTP/SAVPF 0' % default_candidate.port,
+            'c=IN IP4 %s' % default_candidate.host,
+            'a=rtcp:9 IN IP4 0.0.0.0',
         ]
+
         for candidate in self.__iceConnection.local_candidates:
             sdp += ['a=candidate:%s' % candidate.to_sdp()]
         sdp += [
@@ -111,4 +116,14 @@ class RTCPeerConnection:
             'a=ice-ufrag:%s' % self.__iceConnection.local_username,
             'a=fingerprint:sha-256 %s' % self.__dtlsSession.local_fingerprint,
         ]
+        if self.__iceConnection.ice_controlling:
+            sdp += ['a=setup:actpass']
+        else:
+            sdp += ['a=setup:active']
+        sdp += ['a=sendrecv']
+        sdp += ['a=rtcp-mux']
+
+        # FIXME: negotiate codec
+        sdp += ['a=rtpmap:0 PCMU/8000']
+
         return '\r\n'.join(sdp) + '\r\n'
