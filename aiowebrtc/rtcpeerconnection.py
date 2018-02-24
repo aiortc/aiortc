f5f50769660ee3b2fdcc97c81471cdf91e61a55b
Jeremy Lainé <jeremy.laine@m4x.org>
2018-02-24 12:17:24 +0100

    add some docs strings

--- aiowebrtc/rtcpeerconnection.py
+++ aiowebrtc/rtcpeerconnection.py
@@ -28,12 +28,20 @@ class RTCPeerConnection:
         return self.__iceGatheringState
 
     async def createAnswer(self):
+        """
+        Create an SDP answer to an offer received from a remote peer during
+        the offer/answer negotiation of a WebRTC connection.
+        """
         return {
             'sdp': self.__createSdp(),
             'type': 'answer',
         }
 
     async def createOffer(self):
+        """
+        Create an SDP offer for the purpose of starting a new WebRTC
+        connection to a remote peer.
+        """
         self.__iceConnection = aioice.Connection(ice_controlling=True)
         self.__dtlsSession = dtls.DtlsSrtpSession(self.__dtlsContext,
                                                   is_server=True,
