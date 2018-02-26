d0812c2e2c4092c97cfd7e10a794787c55ff1568
Jeremy Lainé <jeremy.laine@m4x.org>
2018-02-26 16:46:51 +0100

    [sctp] fix checksum calculation, parse chunks

--- aiowebrtc/sctp.py
+++ aiowebrtc/sctp.py
@@ -1,4 +1,5 @@
-import struct
+import enum
+from struct import pack, unpack
 
 import crcmod.predefined
 
@@ -6,18 +7,72 @@ import crcmod.predefined
 crc32c = crcmod.predefined.mkPredefinedCrcFun('crc-32c')
 
 
+def decode_params(body):
+    params = []
+    pos = 0
+    while pos < len(body) - 4:
+        tag_type, tag_length = unpack('!HH', body[pos:pos + 4])
+        params.append((tag_type, body[pos + 4:pos + tag_length]))
+        pos += tag_length + padl(tag_length)
+    return params
+
+
+def encode_params(params):
+    body = b''
+    padding = b''
+    for tag_type, tag_value in params:
+        body += padding
+        body += pack('!HH', tag_type, len(tag_value) + 4) + tag_value
+        padding = b'\x00' * padl(len(tag_value))
+    return body
+
+
+def padl(l):
+    return 4 * ((l + 3) // 4) - l
+
+
 def swapl(i):
-    return struct.unpack("<I", struct.pack(">I", i))[0]
+    return unpack("<I", pack(">I", i))[0]
 
 
 class Chunk:
-    def __init__(self, type, flags, data):
+    def __bytes__(self):
+        body = self.body
+        data = pack('!BBH', self.type, self.flags, len(body) + 4) + body
+        data += b'\x00' * padl(len(body))
+        return data
+
+class InitChunk(Chunk):
+    def __init__(self, type, flags, body=None):
         self.type = type
         self.flags = flags
-        self.data = data
+        self.params = []
+        if body:
+            (self.initiate_tag, self.advertise_rwnd, self.outbound_streams,
+             self.inbound_streams, self.initial_tsn) = unpack('!LLHHL', body[0:16])
+            self.params = decode_params(body[16:])
+        else:
+            self.params = []
 
-    def __bytes__(self):
-        return struct.pack('!BBH', self.type, self.flags, len(self.data)) + self.data
+    @property
+    def body(self):
+        body = pack(
+            '!LLHHL', self.initiate_tag, self.advertise_rwnd, self.outbound_streams,
+            self.inbound_streams, self.initial_tsn)
+        body += encode_params(self.params)
+        return body
+
+
+class UnknownChunk(Chunk):
+    def __init__(self, type, flags, body):
+        self.type = type
+        self.flags = flags
+        self.body = body
+        self.params = {}
+
+
+class ChunkType(enum.IntEnum):
+    INIT = 1
 
 
 class Packet:
@@ -29,7 +84,7 @@ class Packet:
 
     def __bytes__(self):
         checksum = 0
-        data = struct.pack(
+        data = pack(
             '!HHII',
             self.source_port,
             self.destination_port,
@@ -37,11 +92,14 @@ class Packet:
             checksum)
         for chunk in self.chunks:
             data += bytes(chunk)
-        return data
+
+        # calculate checksum
+        checksum = swapl(crc32c(data))
+        return data[0:8] + pack('!I', checksum) + data[12:]
 
     @classmethod
     def parse(cls, data):
-        source_port, destination_port, verification_tag, checksum = struct.unpack(
+        source_port, destination_port, verification_tag, checksum = unpack(
             '!HHII', data[0:12])
 
         # verify checksum
@@ -53,13 +111,33 @@ class Packet:
             source_port=source_port,
             destination_port=destination_port,
             verification_tag=verification_tag)
+
         pos = 12
         while pos < len(data) - 4:
-            chunk_type, chunk_flags, chunk_length = struct.unpack('!BBH', data[pos:pos + 4])
-            pos += 4
-            packet.chunks.append(Chunk(
+            chunk_type, chunk_flags, chunk_length = unpack('!BBH', data[pos:pos + 4])
+            chunk_body = data[pos + 4:pos + chunk_length]
+            if chunk_type == ChunkType.INIT:
+                cls = InitChunk
+            else:
+                cls = UnknownChunk
+            packet.chunks.append(cls(
                 type=chunk_type,
                 flags=chunk_flags,
-                data=data[pos:pos + chunk_length]))
-            pos += chunk_length
+                body=chunk_body))
+            pos += chunk_length + padl(chunk_length)
         return packet
+
+
+class Transport:
+    def __init__(self, transport):
+        self.transport = transport
+
+    async def run(self):
+        while True:
+            data = await self.transport.recv()
+            try:
+                packet = Packet.parse(data)
+            except ValueError:
+                continue
+
+            print('FROM port', packet.source_port, 'to', packet.destination_port)
--- tests/test_sctp.py
+++ tests/test_sctp.py
@@ -12,8 +12,16 @@ def load(name):
 
 class SctpPacketTest(TestCase):
     def test_parse(self):
-        data = load('sctp.bin')
+        data = load('sctp_init.bin')
         packet = sctp.Packet.parse(data)
         self.assertEqual(packet.source_port, 5000)
         self.assertEqual(packet.destination_port, 5000)
         self.assertEqual(packet.verification_tag, 0)
+
+        self.assertEqual(len(packet.chunks), 1)
+        self.assertEqual(packet.chunks[0].type, sctp.ChunkType.INIT)
+        self.assertEqual(packet.chunks[0].flags, 0)
+        print(packet.chunks[0].params)
+        self.assertEqual(len(packet.chunks[0].body), 82)
+
+        self.assertEqual(bytes(packet), data)
