ed59961e466293f26d8e1c4df8410eeb5061d72d
Jeremy Lainé <jeremy.laine@m4x.org>
2018-03-03 00:23:42 +0100

    [opus] don't resample decoded data, leave it at 48kHz

--- aiortc/codecs/opus.py
+++ aiortc/codecs/opus.py
@@ -3,52 +3,45 @@ import audioop
 from ..mediastreams import AudioFrame
 from ._opus import ffi, lib
 
+CHANNELS = 2
+FRAME_SIZE = 960
+SAMPLE_RATE = 48000
+SAMPLE_WIDTH = 2
+
 
 class OpusDecoder:
     def __init__(self):
         error = ffi.new('int *')
-        self.channels = 2
-        self.frame_size = 960
-        self.sample_width = 2
-        self.decoder = lib.opus_decoder_create(48000, self.channels, error)
+        self.decoder = lib.opus_decoder_create(SAMPLE_RATE, CHANNELS, error)
         assert error[0] == lib.OPUS_OK
 
-        self.cdata = ffi.new('char []', self.frame_size * self.channels * self.sample_width)
+        self.cdata = ffi.new('char []', FRAME_SIZE * CHANNELS * SAMPLE_WIDTH)
         self.buffer = ffi.buffer(self.cdata)
-        self.rate_state = None
 
     def __del__(self):
         lib.opus_decoder_destroy(self.decoder)
 
     def decode(self, data):
         length = lib.opus_decode(self.decoder, data, len(data),
-                                 ffi.cast('int16_t *', self.cdata), self.frame_size, 0)
-        assert length == self.frame_size
-
-        # resample at 8 kHz
-        data, self.rate_state = audioop.ratecv(
-            self.buffer,
-            self.sample_width,
-            self.channels,
-            48000,
-            8000,
-            self.rate_state)
+                                 ffi.cast('int16_t *', self.cdata), FRAME_SIZE, 0)
+        assert length == FRAME_SIZE
 
         return AudioFrame(
-            channels=2,
-            data=data,
-            sample_rate=8000)
+            channels=CHANNELS,
+            data=self.buffer[:],
+            sample_rate=SAMPLE_RATE)
 
 
 class OpusEncoder:
-    timestamp_increment = 960
+    timestamp_increment = FRAME_SIZE
 
     def __init__(self):
         error = ffi.new('int *')
-        self.encoder = lib.opus_encoder_create(48000, 2, lib.OPUS_APPLICATION_VOIP, error)
+        self.encoder = lib.opus_encoder_create(
+            SAMPLE_RATE, CHANNELS, lib.OPUS_APPLICATION_VOIP, error)
         assert error[0] == lib.OPUS_OK
 
-        self.cdata = ffi.new('char []', 960)
+        self.cdata = ffi.new('char []', FRAME_SIZE * CHANNELS * SAMPLE_WIDTH)
         self.buffer = ffi.buffer(self.cdata)
         self.rate_state = None
 
@@ -59,13 +52,13 @@ class OpusEncoder:
         data = frame.data
 
         # resample at 48 kHz
-        if frame.sample_rate != 48000:
+        if frame.sample_rate != SAMPLE_RATE:
             data, self.rate_state = audioop.ratecv(
                 data,
                 frame.sample_width,
                 frame.channels,
                 frame.sample_rate,
-                48000,
+                SAMPLE_RATE,
                 self.rate_state)
 
         # convert to stereo
@@ -73,6 +66,6 @@ class OpusEncoder:
             data = audioop.tostereo(data, frame.sample_width, 1, 1)
 
         length = lib.opus_encode(self.encoder, ffi.cast('int16_t*', ffi.from_buffer(data)),
-                                 960, self.cdata, len(self.cdata))
+                                 FRAME_SIZE, self.cdata, len(self.cdata))
         assert length > 0
         return self.buffer[0:length]
--- tests/test_opus.py
+++ tests/test_opus.py
@@ -9,13 +9,14 @@ class OpusTest(TestCase):
         decoder = OpusDecoder()
         frame = decoder.decode(b'\xfc\xff\xfe')
         self.assertEqual(frame.channels, 2)
-        self.assertEqual(frame.data, b'\x00' * 4 * 160)
+        self.assertEqual(frame.data, b'\x00' * 4 * 960)
+        self.assertEqual(frame.sample_rate, 48000)
 
     def test_encoder_mono_8khz(self):
         encoder = OpusEncoder()
         frame = AudioFrame(
             channels=1,
-            data=b'\x00' * 2 * 160,
+            data=b'\x00\x00' * 160,
             sample_rate=8000)
         data = encoder.encode(frame)
         self.assertEqual(data, b'\xfc\xff\xfe')
@@ -24,7 +25,16 @@ class OpusTest(TestCase):
         encoder = OpusEncoder()
         frame = AudioFrame(
             channels=2,
-            data=b'\x00' * 4 * 160,
+            data=b'\x00\x00' * 2 * 160,
             sample_rate=8000)
+        data = encoder.encode(frame)
+        self.assertEqual(data, b'\xfc\xff\xfe')
+
+    def test_encoder_stereo_48khz(self):
+        encoder = OpusEncoder()
+        frame = AudioFrame(
+            channels=2,
+            data=b'\x00\x00' * 2 * 960,
+            sample_rate=48000)
         data = encoder.encode(frame)
         self.assertEqual(data, b'\xfc\xff\xfe')
