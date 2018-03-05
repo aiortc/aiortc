6229f241a39a9604d17fce0c6026de48ed97c6a7
Jeremy Lainé <jeremy.laine@m4x.org>
2018-03-05 09:51:42 +0100

    [tests] add test for receiving RTP / RTCP

--- tests/test_rtcrtptransceiver.py
+++ tests/test_rtcrtptransceiver.py
@@ -5,7 +5,7 @@ from aiortc.codecs.g711 import PcmuDecoder, PcmuEncoder
 from aiortc.mediastreams import AudioStreamTrack
 from aiortc.rtcrtptransceiver import RTCRtpReceiver, RTCRtpSender
 
-from .utils import dummy_transport_pair, run
+from .utils import dummy_transport_pair, load, run
 
 
 class RTCRtpReceiverTest(TestCase):
@@ -17,6 +17,24 @@ class RTCRtpReceiverTest(TestCase):
         run(asyncio.gather(
             receiver._run(transport=transport, decoder=decoder, payload_type=0),
             transport.close()))
+
+    def test_rtp_and_rtcp(self):
+        transport, remote = dummy_transport_pair()
+        decoder = PcmuDecoder()
+
+        receiver = RTCRtpReceiver()
+        task = asyncio.ensure_future(
+            receiver._run(transport=transport, decoder=decoder, payload_type=0))
+
+        # receive RTP
+        run(remote.send(load('rtp.bin')))
+
+        # receive RTCP
+        run(remote.send(load('rtcp_sr.bin')))
+
+        # shutdown
+        run(transport.close())
+        run(task)
 
 
 class RTCRtpSenderTest(TestCase):
