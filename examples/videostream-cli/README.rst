3d7d471eb7dca90ecb872e0a38619829493ea2a4
Jeremy Lainé <jeremy.laine@m4x.org>
2018-04-10 23:43:39 +0200

    [examples] add a video stream cli example
    This is based on the work of Nick Knudson, as submitted in PR #11.

---
+++ examples/videostream-cli/README.rst
@@ -0,0 +1,32 @@
+Video channel CLI
+=================
+
+This example illustrates the establishment of a video stream using an
+RTCPeerConnection and a "copy and paste" signaling channel to exchange SDP.
+
+The video stream is composed of three separate streams that have been combined
+into a single stream that is three times as wide.
+
+First install dependencies:
+
+.. code:: bash
+
+   pip install numpy opencv-python
+
+To run the example, you will need instances of the `cli` example:
+
+- The first takes on the role of the offerer. It generates an offer which you
+  must copy-and-paste to the answerer.
+
+.. code:: bash
+
+   python cli.py offer
+
+- The second takes on the role of the answerer. When given an offer, it will
+  generate an answer which you must copy-and-paste to the offerer.
+
+.. code:: bash
+
+   python cli.py answer
+
+Check `output.png` for the result of the stream.
---
+++ examples/videostream-cli/cli.py
@@ -0,0 +1,142 @@
+import argparse
+import asyncio
+import json
+import logging
+import math
+import os
+
+import cv2
+import numpy
+
+from aiortc import RTCPeerConnection, RTCSessionDescription
+from aiortc.mediastreams import VideoFrame, VideoStreamTrack
+
+BLUE = (255, 0, 0)
+GREEN = (0, 255, 0)
+RED = (0, 0, 255)
+
+OUTPUT_PATH = os.path.join(os.path.dirname(__file__), 'output.png')
+
+
+def frame_from_bgr(data_bgr):
+    data_yuv = cv2.cvtColor(data_bgr, cv2.COLOR_BGR2YUV_YV12)
+    return VideoFrame(width=data_bgr.shape[1], height=data_bgr.shape[0], data=data_yuv.tobytes())
+
+
+def frame_to_bgr(frame):
+    data_flat = numpy.frombuffer(frame.data, numpy.uint8)
+    data_yuv = data_flat.reshape((math.ceil(frame.height * 12 / 8), frame.width))
+    return cv2.cvtColor(data_yuv, cv2.COLOR_YUV2BGR_YV12)
+
+
+class ColorVideoStreamTrack(VideoStreamTrack):
+    def __init__(self, width, height, color):
+        data_bgr = numpy.zeros((height, width, 3), numpy.uint8)
+        data_bgr[:, :] = color
+        self.frame = frame_from_bgr(data_bgr=data_bgr)
+
+    async def recv(self):
+        return self.frame
+
+
+class CombinedVideoStreamTrack(VideoStreamTrack):
+    def __init__(self, tracks):
+        self.tracks = tracks
+
+    async def recv(self):
+        frames = [await track.recv() for track in self.tracks]
+        data_bgrs = [frame_to_bgr(frame) for frame in frames]
+        data_bgr = numpy.hstack(data_bgrs)
+        return frame_from_bgr(data_bgr)
+
+
+async def run_answer(pc):
+    remote_track = None
+
+    @pc.on('track')
+    def on_track(track):
+        nonlocal remote_track
+        assert track.kind == 'video'
+        remote_track = track
+
+    # receive offer
+    print('-- Please enter remote offer --')
+    offer_json = json.loads(input())
+    await pc.setRemoteDescription(RTCSessionDescription(
+        sdp=offer_json['sdp'],
+        type=offer_json['type']))
+    print()
+
+    # send answer
+    await pc.setLocalDescription(await pc.createAnswer())
+    answer = pc.localDescription
+    print('-- Your answer --')
+    print(json.dumps({
+        'sdp': answer.sdp,
+        'type': answer.type
+    }))
+    print()
+
+    print('Receiving video, press CTRL-C to stop')
+    while True:
+        frame = await remote_track.recv()
+        data_bgr = frame_to_bgr(frame)
+        cv2.imwrite(OUTPUT_PATH, data_bgr)
+
+
+async def run_offer(pc):
+    # add video track
+    width = 320
+    height = 240
+    local_video = CombinedVideoStreamTrack(tracks=[
+        ColorVideoStreamTrack(width=width, height=height, color=BLUE),
+        ColorVideoStreamTrack(width=width, height=height, color=GREEN),
+        ColorVideoStreamTrack(width=width, height=height, color=RED),
+    ])
+    pc.addTrack(local_video)
+
+    # send offer
+    await pc.setLocalDescription(await pc.createOffer())
+    offer = pc.localDescription
+    print('-- Your offer --')
+    print(json.dumps({
+        'sdp': offer.sdp,
+        'type': offer.type
+    }))
+    print()
+
+    # receive answer
+    print('-- Please enter remote answer --')
+    answer_json = json.loads(input())
+    await pc.setRemoteDescription(RTCSessionDescription(
+        sdp=answer_json['sdp'],
+        type=answer_json['type']))
+    print()
+
+    print('Sending video for 10s')
+    await asyncio.sleep(10)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Video stream with copy-and-paste signaling')
+    parser.add_argument('role', choices=['offer', 'answer'])
+    parser.add_argument('--verbose', '-v', action='count')
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+
+    pc = RTCPeerConnection()
+    if args.role == 'offer':
+        coro = run_offer(pc)
+    else:
+        coro = run_answer(pc)
+
+    # run event loop
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(coro)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        loop.run_until_complete(pc.close())
