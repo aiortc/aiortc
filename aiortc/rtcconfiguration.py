17c8ae97c706ba05c053198d6d30f7c7a071e045
Victor Duarte da Silva <ds.victor94@gmail.com>
2018-06-03 19:14:08 -0300

    [peer connection] fix iceServers attr

--- aiortc/rtcconfiguration.py
+++ aiortc/rtcconfiguration.py
@@ -4,7 +4,8 @@ import attr
 @attr.s
 class RTCConfiguration:
     bundlePolicy = attr.ib(default='max-compat')
-    iceServers = attr.ib(factory=lambda: [RTCIceServer('stun:stun.l.google.com:19302')])
+    iceServers = attr.ib(
+        default=attr.Factory(lambda: [RTCIceServer('stun:stun.l.google.com:19302')]))
 
 
 @attr.s
