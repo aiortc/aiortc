af4127350c78e6334c00206473b6d671edb80aaa
Jeremy Lainé <jeremy.laine@m4x.org>
2018-06-15 20:28:35 +0200

    [examples] split javascript out to a separate file

---
+++ examples/server/client.js
@@ -0,0 +1,119 @@
+var pc = new RTCPeerConnection();
+
+// get DOM elements
+var dataChannelLog = document.getElementById('data-channel'),
+    iceConnectionLog = document.getElementById('ice-connection-state'),
+    iceGatheringLog = document.getElementById('ice-gathering-state'),
+    signalingLog = document.getElementById('signaling-state');
+
+// register some listeners to help debugging
+pc.addEventListener('icegatheringstatechange', function() {
+    iceGatheringLog.textContent += ' -> ' + pc.iceGatheringState;
+}, false);
+iceGatheringLog.textContent = pc.iceGatheringState;
+
+pc.addEventListener('iceconnectionstatechange', function() {
+    iceConnectionLog.textContent += ' -> ' + pc.iceConnectionState;
+}, false);
+iceConnectionLog.textContent = pc.iceConnectionState;
+
+pc.addEventListener('signalingstatechange', function() {
+    signalingLog.textContent += ' -> ' + pc.signalingState;
+}, false);
+signalingLog.textContent = pc.signalingState;
+
+// connect audio / video
+pc.addEventListener('track', function(evt) {
+    if (evt.track.kind == 'video')
+        document.getElementById('video').srcObject = evt.streams[0];
+    else
+        document.getElementById('audio').srcObject = evt.streams[0];
+});
+
+// data channel
+var dc = null, dcInterval;
+
+function negotiate() {
+    return pc.createOffer().then(function(offer) {
+        return pc.setLocalDescription(offer);
+    }).then(function() {
+        // wait for ICE gathering to complete
+        return new Promise(function(resolve) {
+            if (pc.iceGatheringState === 'complete') {
+                resolve();
+            } else {
+                function checkState() {
+                    if (pc.iceGatheringState === 'complete') {
+                        pc.removeEventListener('icegatheringstatechange', checkState);
+                        resolve();
+                    }
+                }
+                pc.addEventListener('icegatheringstatechange', checkState);
+            }
+        });
+    }).then(function() {
+        var offer = pc.localDescription;
+        document.getElementById('offer-sdp').textContent = offer.sdp;
+        return fetch('/offer', {
+            body: JSON.stringify(offer),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: 'POST'
+        });
+    }).then(function(response) {
+        return response.json();
+    }).then(function(answer) {
+        document.getElementById('answer-sdp').textContent = answer.sdp;
+        return pc.setRemoteDescription(answer);
+    });
+}
+
+function start() {
+    document.getElementById('start').style.display = 'none';
+
+    dc = pc.createDataChannel('chat');
+    dc.onmessage = function(evt) {
+        dataChannelLog.textContent += '< ' + evt.data + '\n';
+    };
+
+    var constraints = {
+        audio: document.getElementById('use-audio').checked,
+        video: document.getElementById('use-video').checked
+    };
+
+    var promise;
+    if (constraints.audio || constraints.video) {
+        if (constraints.video) {
+            document.getElementById('media').style.display = 'block';
+        }
+        promise = navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+            stream.getTracks().forEach(function(track) {
+                pc.addTrack(track, stream);
+            });
+            return negotiate();
+        });
+    } else {
+        promise = negotiate();
+    }
+
+    promise.then(function() {
+        dcInterval = setInterval(function() {
+            var message = 'ping';
+            dataChannelLog.textContent += '> ' + message + '\n';
+            dc.send(message);
+        }, 1000);
+    });
+
+    document.getElementById('stop').style.display = 'inline-block';
+}
+
+function stop() {
+    document.getElementById('stop').style.display = 'none';
+
+    clearInterval(dcInterval);
+    pc.getSenders().forEach(function(sender) {
+        sender.track.stop();
+    });
+    pc.close();
+}
--- examples/server/server.py
+++ examples/server/server.py
@@ -84,8 +84,13 @@ async def consume_video(track, local_video):
 
 
 async def index(request):
-    html = open(os.path.join(ROOT, 'index.html'), 'r').read()
-    return web.Response(content_type='text/html', text=html)
+    content = open(os.path.join(ROOT, 'index.html'), 'r').read()
+    return web.Response(content_type='text/html', text=content)
+
+
+async def javascript(request):
+    content = open(os.path.join(ROOT, 'client.js'), 'r').read()
+    return web.Response(content_type='application/javascript', text=content)
 
 
 async def offer(request):
@@ -156,5 +161,6 @@ if __name__ == '__main__':
     app = web.Application()
     app.on_shutdown.append(on_shutdown)
     app.router.add_get('/', index)
+    app.router.add_get('/client.js', javascript)
     app.router.add_post('/offer', offer)
     web.run_app(app, port=args.port)
