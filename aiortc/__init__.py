2fc766ab9e58077d43e192a48107cc8e70c2f522
Jeremy Lainé <jeremy.laine@m4x.org>
2018-06-25 21:38:23 +0200

    [package] make sure RTCRtpCodecParameters is exported

--- aiortc/__init__.py
+++ aiortc/__init__.py
@@ -7,7 +7,8 @@ from .rtcdtlstransport import (RTCCertificate, RTCDtlsFingerprint,  # noqa
 from .rtcicetransport import (RTCIceCandidate, RTCIceGatherer,  # noqa
                               RTCIceParameters, RTCIceTransport)
 from .rtcpeerconnection import RTCPeerConnection  # noqa
-from .rtcrtpparameters import RTCRtcpParameters, RTCRtpParameters  # noqa
+from .rtcrtpparameters import (RTCRtcpParameters,  # noqa
+                               RTCRtpCodecParameters, RTCRtpParameters)
 from .rtcrtpreceiver import RTCRtpReceiver  # noqa
 from .rtcrtpsender import RTCRtpSender  # noqa
 from .rtcrtptransceiver import RTCRtpTransceiver  # noqa
