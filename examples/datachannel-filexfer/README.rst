a45786d31ec21b2ae4cbe3daf901a0cd2d98de08
Jeremy Lainé <jeremy.laine@m4x.org>
2018-07-12 23:49:10 +0200

    [examples] add an example of file transfer over a data channel

---
+++ examples/datachannel-filexfer/README.rst
@@ -0,0 +1,27 @@
+Data channel file transfer
+==========================
+
+This example illustrates sending a file over a data channel using an
+RTCPeerConnection and a "copy and paste" signaling channel to exchange SDP.
+
+First install the required packages:
+
+.. code-block:: console
+
+    $ pip install aiortc
+
+To run the example, you will need instances of the `filexfer` example:
+
+- The first takes on the role of the offerer. It generates an offer which you
+  must copy-and-paste to the answerer.
+
+.. code-block:: console
+
+   $ python cli.py send somefile.pdf
+
+- The second takes on the role of the answerer. When given an offer, it will
+  generate an answer which you must copy-and-paste to the offerer.
+
+.. code-block:: console
+
+   $ python cli.py receive received.pdf
---
+++ examples/datachannel-filexfer/filexfer.py
@@ -0,0 +1,115 @@
+import argparse
+import asyncio
+import json
+import logging
+
+from aiortc import RTCPeerConnection, RTCSessionDescription
+
+
+def channel_log(channel, t, message):
+    print('channel(%s) %s %s' % (channel.label, t, message))
+
+
+async def run_answer(pc, filename):
+    done = asyncio.Event()
+
+    @pc.on('datachannel')
+    def on_datachannel(channel):
+        fp = open(filename, 'wb')
+
+        @channel.on('message')
+        def on_message(message):
+            if message:
+                fp.write(message)
+            else:
+                fp.close()
+                channel.send('done')
+                done.set()
+
+    # receive offer
+    print('-- Please enter remote offer --')
+    offer_json = json.loads(input())
+    await pc.setRemoteDescription(RTCSessionDescription(
+        sdp=offer_json['sdp'],
+        type=offer_json['type']))
+    print()
+
+    # send answer
+    await pc.setLocalDescription(await pc.createAnswer())
+    answer = pc.localDescription
+    print('-- Your answer --')
+    print(json.dumps({
+        'sdp': answer.sdp,
+        'type': answer.type
+    }))
+    print()
+
+    await done.wait()
+
+
+async def run_offer(pc, filename):
+    done = asyncio.Event()
+
+    channel = pc.createDataChannel('filexfer')
+    channel_log(channel, '-', 'created by local party')
+
+    @channel.on('message')
+    def on_message(message):
+        # quit
+        done.set()
+
+    # send offer
+    await pc.setLocalDescription(await pc.createOffer())
+    offer = pc.localDescription
+    print('-- Your offer --')
+    print(json.dumps({
+        'sdp': offer.sdp,
+        'type': offer.type
+    }))
+    print()
+
+    # receive answer
+    print('-- Please enter remote answer --')
+    answer_json = json.loads(input())
+    await pc.setRemoteDescription(RTCSessionDescription(
+        sdp=answer_json['sdp'],
+        type=answer_json['type']))
+    print()
+
+    # send file
+    with open(filename, 'rb') as fp:
+        while True:
+            data = fp.read(4096)
+            if data:
+                channel.send(data)
+            else:
+                channel.send(b'')
+                break
+
+    await done.wait()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Data channel file transfer')
+    parser.add_argument('role', choices=['send', 'receive'])
+    parser.add_argument('filename')
+    parser.add_argument('--verbose', '-v', action='count')
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+
+    pc = RTCPeerConnection()
+    if args.role == 'send':
+        coro = run_offer(pc, args.filename)
+    else:
+        coro = run_answer(pc, args.filename)
+
+    # run event loop
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(coro)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        loop.run_until_complete(pc.close())
