fcdcae79e7aa0b80f85cfe5cbae72a6c03e54584
Jeremy Lainé <jeremy.laine@m4x.org>
2018-08-17 16:47:21 +0200

    [tests] create WAV file ourselves instead of relying on examples

--- tests/test_contrib_media.py
+++ tests/test_contrib_media.py
@@ -1,11 +1,23 @@
 import os
+import wave
 from unittest import TestCase
 
 import cv2
 import numpy
+
 from aiortc.contrib.media import AudioFileTrack, VideoFileTrack
 
 from .utils import run
+
+
+def create_audio(path, channels=1, sample_rate=8000, sample_width=2):
+    writer = wave.open(path, 'wb')
+    writer.setnchannels(channels)
+    writer.setframerate(sample_rate)
+    writer.setsampwidth(sample_width)
+
+    writer.writeframes(b'\x00\x00' * sample_rate)
+    writer.close()
 
 
 def create_video(path, width=640, height=480, fps=20, duration=1):
@@ -23,13 +35,14 @@ def create_video(path, width=640, height=480, fps=20, duration=1):
 
 class FileTrackTest(TestCase):
     def setUp(self):
-        self.audio_path = os.path.join(
-            os.path.dirname(__file__), os.path.pardir, 'examples', 'server', 'demo-instruct.wav')
+        self.audio_path = os.path.join(os.path.dirname(__file__), 'test.wav')
+        create_audio(self.audio_path)
 
         self.video_path = os.path.join(os.path.dirname(__file__), 'test.avi')
         create_video(self.video_path)
 
     def tearDown(self):
+        os.unlink(self.audio_path)
         os.unlink(self.video_path)
 
     def test_audio_file_track(self):
