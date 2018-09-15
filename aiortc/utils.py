ea4293211a45e27a1b8e87460e2e933d8f356a50
Jeremy Lainé <jeremy.laine@m4x.org>
2018-09-15 22:10:05 +0200

    [utils] add some type annotations

--- aiortc/utils.py
+++ aiortc/utils.py
@@ -3,22 +3,22 @@ import os
 from struct import unpack
 
 
-def random16():
+def random16() -> int:
     return unpack('!H', os.urandom(2))[0]
 
 
-def random32():
+def random32() -> int:
     return unpack('!L', os.urandom(4))[0]
 
 
-def uint16_add(a, b):
+def uint16_add(a: int, b: int) -> int:
     """
     Return a + b.
     """
     return (a + b) & 0xFFFF
 
 
-def uint16_gt(a, b):
+def uint16_gt(a: int, b: int) -> bool:
     """
     Return a > b.
     """
@@ -27,21 +27,21 @@ def uint16_gt(a, b):
             ((a > b) and ((a - b) < half_mod)))
 
 
-def uint16_gte(a, b):
+def uint16_gte(a: int, b: int) -> bool:
     """
     Return a >= b.
     """
     return (a == b) or uint16_gt(a, b)
 
 
-def uint32_add(a, b):
+def uint32_add(a: int, b: int) -> int:
     """
     Return a + b.
     """
     return (a + b) & 0xFFFFFFFF
 
 
-def uint32_gt(a, b):
+def uint32_gt(a: int, b: int) -> bool:
     """
     Return a > b.
     """
@@ -50,7 +50,7 @@ def uint32_gt(a, b):
             ((a > b) and ((a - b) < half_mod)))
 
 
-def uint32_gte(a, b):
+def uint32_gte(a: int, b: int) -> bool:
     """
     Return a >= b.
     """
