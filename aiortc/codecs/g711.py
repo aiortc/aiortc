1c923fd1cd85f7a7ba0dd845405fc9292f2e8223
Jeremy Lainé <jeremy.laine@m4x.org>
2018-10-03 10:24:26 +0200

    [g711] refactor PCMA / PCMU common code

--- aiortc/codecs/g711.py
+++ aiortc/codecs/g711.py
@@ -7,59 +7,55 @@ SAMPLE_RATE = 8000
 TIME_BASE = fractions.Fraction(1, 8000)
 
 
-def mono_8khz(frame):
-    data = frame.data
-    timestamp = frame.pts
-
-    # resample at 8 kHz
-    if frame.sample_rate != SAMPLE_RATE:
-        data, _ = audioop.ratecv(
-            data,
-            frame.sample_width,
-            frame.channels,
-            frame.sample_rate,
-            SAMPLE_RATE,
-            None)
-        timestamp = (timestamp * SAMPLE_RATE) // frame.sample_rate
-
-    # convert to mono
-    if frame.channels == 2:
-        data = audioop.tomono(data, frame.sample_width, 1, 1)
-
-    return data, timestamp
-
-
-class PcmaDecoder:
+class PcmDecoder:
     def decode(self, encoded_frame):
         frame = AudioFrame(
             channels=1,
-            data=audioop.alaw2lin(encoded_frame.data, 2),
+            data=self._convert(encoded_frame.data, 2),
             sample_rate=SAMPLE_RATE)
         frame.pts = encoded_frame.timestamp
         frame.time_base = TIME_BASE
         return [frame]
 
 
-class PcmaEncoder:
+class PcmEncoder:
+    def __init__(self):
+        self.rate_state = None
+
     def encode(self, frame, force_keyframe=False):
-        data, timestamp = mono_8khz(frame)
-        data = audioop.lin2alaw(data, frame.sample_width)
+        data = frame.data
+        timestamp = frame.pts
+
+        # resample at 8 kHz
+        if frame.sample_rate != SAMPLE_RATE:
+            data, self.rate_state = audioop.ratecv(
+                data,
+                frame.sample_width,
+                frame.channels,
+                frame.sample_rate,
+                SAMPLE_RATE,
+                self.rate_state)
+            timestamp = (timestamp * SAMPLE_RATE) // frame.sample_rate
+
+        # convert to mono
+        if frame.channels == 2:
+            data = audioop.tomono(data, frame.sample_width, 1, 1)
+
+        data = self._convert(data, frame.sample_width)
         return [data], timestamp
 
 
-class PcmuDecoder:
-    def decode(self, encoded_frame):
-        frame = AudioFrame(
-            channels=1,
-            data=audioop.ulaw2lin(encoded_frame.data, 2),
-            sample_rate=SAMPLE_RATE)
-        frame.pts = encoded_frame.timestamp
-        frame.time_base = TIME_BASE
-        return [frame]
+class PcmaEncoder(PcmEncoder):
+    _convert = audioop.lin2alaw
 
 
-class PcmuEncoder:
-    def encode(self, frame, force_keyframe=False):
-        data, timestamp = mono_8khz(frame)
-        data = audioop.lin2ulaw(data, frame.sample_width)
-        return [data], timestamp
+class PcmaDecoder(PcmDecoder):
+    _convert = audioop.alaw2lin
+
+
+class PcmuDecoder(PcmDecoder):
+    _convert = audioop.ulaw2lin
+
+
+class PcmuEncoder(PcmEncoder):
+    _convert = audioop.lin2ulaw
