6c0dc49e302c4687ba6de929e12bd5c0a2a98bdd
Jeremy Lainé <jeremy.laine@m4x.org>
2018-11-20 20:33:12 +0100

    [examples] add command line options for SSL (fixes: #97)

--- examples/server/server.py
+++ examples/server/server.py
@@ -3,6 +3,7 @@ import asyncio
 import json
 import logging
 import os
+import ssl
 
 import cv2
 from aiohttp import web
@@ -134,6 +135,8 @@ async def on_shutdown(app):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='WebRTC audio / video / data-channels demo')
+    parser.add_argument('--cert-file', help='SSL certificate file (for HTTPS)')
+    parser.add_argument('--key-file', help='SSL key file (for HTTPS)')
     parser.add_argument('--port', type=int, default=8080,
                         help='Port for HTTP server (default: 8080)')
     parser.add_argument('--verbose', '-v', action='count')
@@ -143,9 +146,15 @@ if __name__ == '__main__':
     if args.verbose:
         logging.basicConfig(level=logging.DEBUG)
 
+    if args.cert_file:
+        ssl_context = ssl.SSLContext()
+        ssl_context.load_cert_chain(args.cert_file, args.key_file)
+    else:
+        ssl_context = None
+
     app = web.Application()
     app.on_shutdown.append(on_shutdown)
     app.router.add_get('/', index)
     app.router.add_get('/client.js', javascript)
     app.router.add_post('/offer', offer)
-    web.run_app(app, port=args.port)
+    web.run_app(app, port=args.port, ssl_context=ssl_context)
