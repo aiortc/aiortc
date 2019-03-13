2103f08df914c1b61fad1af32e655aff6cee4619
Jeremy Lainé <jeremy.laine@m4x.org>
2019-03-13 20:45:42 +0100

    [packet] define more frame types

--- aioquic/packet.py
+++ aioquic/packet.py
@@ -234,14 +234,29 @@ def push_quic_transport_parameters(buf, params, is_client):
 
 
 class QuicFrameType(IntEnum):
-    PADDING = 0
-    PING = 1
-    ACK = 2
-    ACK_WITH_ECN = 3
-    RESET_STREAM = 4
-    STOP_SENDING = 5
-    CRYPTO = 6
+    PADDING = 0x00
+    PING = 0x01
+    ACK = 0x02
+    ACK_ECN = 0x03
+    RESET_STREAM = 0x04
+    STOP_SENDING = 0x05
+    CRYPTO = 0x06
+    NEW_TOKEN = 0x07
+    STREAM_BASE = 0x08
+    MAX_DATA = 0x10
+    MAX_STREAM_DATA = 0x11
+    MAX_STREAMS_BIDI = 0x12
+    MAX_STREAMS_UNI = 0x13
+    DATA_BLOCKED = 0x14
+    STREAM_DATA_BLOCKED = 0x15
+    STREAMS_BLOCKED_BIDI = 0x16
+    STREAMS_BLOCKED_UNI = 0x17
     NEW_CONNECTION_ID = 0x18
+    RETIRE_CONNECTION_ID = 0x19
+    PATH_CHALLENGE = 0x1a
+    PATH_RESPONSE = 0x1b
+    TRANSPORT_CLOSE = 0x1c
+    APPLICATION_CLOSE = 0x1d
 
 
 def pull_ack_frame(buf):
