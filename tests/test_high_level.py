b41523760ea7198d2ff021c0506dd3e76360ca88
Jeremy Lainé <jeremy.laine@m4x.org>
2019-05-22 21:33:30 +0200

    [tests] add high-level test for session resumption

--- tests/test_high_level.py
+++ tests/test_high_level.py
@@ -7,6 +7,17 @@ from aioquic.server import serve
 from .utils import SERVER_CERTIFICATE, SERVER_PRIVATE_KEY, run
 
 
+class SessionTicketStore:
+    def __init__(self):
+        self.tickets = {}
+
+    def add(self, ticket):
+        self.tickets[ticket.ticket] = ticket
+
+    def pop(self, label):
+        return self.tickets.pop(label, None)
+
+
 async def run_client(host, **kwargs):
     async with connect(host, 4433, **kwargs) as client:
         reader, writer = await client.create_stream()
@@ -26,14 +37,14 @@ def handle_stream(reader, writer):
     asyncio.ensure_future(serve())
 
 
-async def run_server(stateless_retry):
+async def run_server(**kwargs):
     await serve(
         host="::",
         port="4433",
         certificate=SERVER_CERTIFICATE,
         private_key=SERVER_PRIVATE_KEY,
-        stateless_retry=stateless_retry,
         stream_handler=handle_stream,
+        **kwargs
     )
 
 
@@ -41,6 +52,33 @@ class HighLevelTest(TestCase):
     def test_connect_and_serve(self):
         _, response = run(
             asyncio.gather(run_server(stateless_retry=False), run_client("127.0.0.1"))
+        )
+        self.assertEqual(response, b"gnip")
+
+    def test_connect_and_serve_with_session_ticket(self):
+        client_ticket = None
+        store = SessionTicketStore()
+
+        def save_ticket(t):
+            nonlocal client_ticket
+            client_ticket = t
+
+        # first request
+        _, response = run(
+            asyncio.gather(
+                run_server(session_ticket_handler=store.add),
+                run_client("127.0.0.1", session_ticket_handler=save_ticket),
+            )
+        )
+        self.assertEqual(response, b"gnip")
+
+        self.assertIsNotNone(client_ticket)
+
+        # second request
+        _, response = run(
+            asyncio.gather(
+                run_server(session_ticket_fetcher=store.pop), run_client("127.0.0.1")
+            )
         )
         self.assertEqual(response, b"gnip")
 
