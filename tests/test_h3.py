c1db156be227ef336f3233f50327ec0fa96faef1
Jeremy Lainé <jeremy.laine@m4x.org>
2019-07-21 08:26:58 +0200

    [http3] test using dynamic tables

--- tests/test_h3.py
+++ tests/test_h3.py
@@ -20,6 +20,83 @@ def h3_transfer(quic_sender, h3_receiver):
 
 
 class H3ConnectionTest(TestCase):
+    def _make_request(self, h3_client, h3_server):
+        quic_client = h3_client._quic
+        quic_server = h3_server._quic
+
+        # send request
+        stream_id = quic_client.get_next_available_stream_id()
+        h3_client.send_headers(
+            stream_id=stream_id,
+            headers=[
+                (b":method", b"GET"),
+                (b":scheme", b"https"),
+                (b":authority", b"localhost"),
+                (b":path", b"/"),
+                (b"x-foo", b"client"),
+            ],
+        )
+        h3_client.send_data(stream_id=stream_id, data=b"", end_stream=True)
+
+        # receive request
+        events = h3_transfer(quic_client, h3_server)
+        self.assertEqual(len(events), 2)
+
+        self.assertTrue(isinstance(events[0], RequestReceived))
+        self.assertEqual(
+            events[0].headers,
+            [
+                (b":method", b"GET"),
+                (b":scheme", b"https"),
+                (b":authority", b"localhost"),
+                (b":path", b"/"),
+                (b"x-foo", b"client"),
+            ],
+        )
+        self.assertEqual(events[0].stream_id, stream_id)
+        self.assertEqual(events[0].stream_ended, False)
+
+        self.assertTrue(isinstance(events[1], DataReceived))
+        self.assertEqual(events[1].data, b"")
+        self.assertEqual(events[1].stream_id, stream_id)
+        self.assertEqual(events[1].stream_ended, True)
+
+        # send response
+        h3_server.send_headers(
+            stream_id=stream_id,
+            headers=[
+                (b":status", b"200"),
+                (b"content-type", b"text/html; charset=utf-8"),
+                (b"x-foo", b"server"),
+            ],
+        )
+        h3_server.send_data(
+            stream_id=stream_id,
+            data=b"<html><body>hello</body></html>",
+            end_stream=True,
+        )
+
+        # receive response
+        events = h3_transfer(quic_server, h3_client)
+        self.assertEqual(len(events), 2)
+
+        self.assertTrue(isinstance(events[0], ResponseReceived))
+        self.assertEqual(
+            events[0].headers,
+            [
+                (b":status", b"200"),
+                (b"content-type", b"text/html; charset=utf-8"),
+                (b"x-foo", b"server"),
+            ],
+        )
+        self.assertEqual(events[0].stream_id, stream_id)
+        self.assertEqual(events[0].stream_ended, False)
+
+        self.assertTrue(isinstance(events[1], DataReceived))
+        self.assertEqual(events[1].data, b"<html><body>hello</body></html>")
+        self.assertEqual(events[1].stream_id, stream_id)
+        self.assertEqual(events[1].stream_ended, True)
+
     def test_connect(self):
         with client_and_server(
             client_options={"alpn_protocols": ["h3-22"]},
@@ -28,71 +105,14 @@ class H3ConnectionTest(TestCase):
             h3_client = H3Connection(quic_client)
             h3_server = H3Connection(quic_server)
 
-            # send request
-            stream_id = quic_client.get_next_available_stream_id()
-            h3_client.send_headers(
-                stream_id=stream_id,
-                headers=[
-                    (b":method", b"GET"),
-                    (b":scheme", b"https"),
-                    (b":authority", b"localhost"),
-                    (b":path", b"/"),
-                ],
-            )
-            h3_client.send_data(stream_id=stream_id, data=b"", end_stream=True)
+            # make first request
+            self._make_request(h3_client, h3_server)
 
-            # receive request
-            events = h3_transfer(quic_client, h3_server)
-            self.assertEqual(len(events), 2)
+            # make second request
+            self._make_request(h3_client, h3_server)
 
-            self.assertTrue(isinstance(events[0], RequestReceived))
-            self.assertEqual(
-                events[0].headers,
-                [
-                    (b":method", b"GET"),
-                    (b":scheme", b"https"),
-                    (b":authority", b"localhost"),
-                    (b":path", b"/"),
-                ],
-            )
-            self.assertEqual(events[0].stream_id, stream_id)
-            self.assertEqual(events[0].stream_ended, False)
-
-            self.assertTrue(isinstance(events[1], DataReceived))
-            self.assertEqual(events[1].data, b"")
-            self.assertEqual(events[1].stream_id, stream_id)
-            self.assertEqual(events[1].stream_ended, True)
-
-            # send response
-            h3_server.send_headers(
-                stream_id=stream_id,
-                headers=[
-                    (b":status", b"200"),
-                    (b"content-type", b"text/html; charset=utf-8"),
-                ],
-            )
-            h3_server.send_data(
-                stream_id=stream_id,
-                data=b"<html><body>hello</body></html>",
-                end_stream=True,
-            )
-
-            # receive response
-            events = h3_transfer(quic_server, h3_client)
-            self.assertEqual(len(events), 2)
-
-            self.assertTrue(isinstance(events[0], ResponseReceived))
-            self.assertEqual(
-                events[0].headers,
-                [(b":status", b"200"), (b"content-type", b"text/html; charset=utf-8")],
-            )
-            self.assertEqual(events[0].stream_id, stream_id)
-            self.assertEqual(events[0].stream_ended, False)
-
-            self.assertTrue(isinstance(events[1], DataReceived))
-            self.assertEqual(events[1].data, b"<html><body>hello</body></html>")
-            self.assertEqual(events[1].stream_id, stream_id)
-            self.assertEqual(events[1].stream_ended, True)
+            # make third request -> dynamic table
+            self._make_request(h3_client, h3_server)
 
     def test_uni_stream_type(self):
         with client_and_server(
