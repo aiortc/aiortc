7365d96287a87424b98a3f4c17a13f5ca57c3d1c
Jeremy Lainé <jeremy.laine@m4x.org>
2019-09-12 10:47:53 +0200

    [tests] use valid headers for test vectors

--- tests/test_crypto.py
+++ tests/test_crypto.py
@@ -14,17 +14,17 @@ from aioquic.tls import CipherSuite
 
 CHACHA20_CLIENT_PACKET_NUMBER = 2
 CHACHA20_CLIENT_PLAIN_HEADER = binascii.unhexlify(
-    "e1ff000014f5b06e20f064d8783dfab56c61e5e16e8024c0e1d6ddc2a43565a240170002"
+    "e1ff0000160880b57c7b70d8524b0850fc2a28e240fd7640170002"
 )
-CHACHA20_CLIENT_PLAIN_PAYLOAD = binascii.unhexlify("0200000000")
+CHACHA20_CLIENT_PLAIN_PAYLOAD = binascii.unhexlify("0201000000")
 CHACHA20_CLIENT_ENCRYPTED_PACKET = binascii.unhexlify(
-    "e7ff000014f5b06e20f064d8783dfab56c61e5e16e8024c0e1d6ddc2a43565a240175554"
-    "c9ead500f378c5b1dd3eebab26c089128698919bee"
+    "e8ff0000160880b57c7b70d8524b0850fc2a28e240fd7640178313b04be98449"
+    "eb10567e25ce930381f2a5b7da2db8db"
 )
 
 LONG_CLIENT_PACKET_NUMBER = 2
 LONG_CLIENT_PLAIN_HEADER = binascii.unhexlify(
-    "c3ff000012508394c8f03e51570800449f00000002"
+    "c3ff000017088394c8f03e5157080000449e00000002"
 )
 LONG_CLIENT_PLAIN_PAYLOAD = binascii.unhexlify(
     "060040c4010000c003036660261ff947cea49cce6cfad687f457cf1b14531ba1"
@@ -34,50 +34,52 @@ LONG_CLIENT_PLAIN_PAYLOAD = binascii.unhexlify(
     "2aedce005ff183d7bb1495207236647037002b0003020304000d0020001e0403"
     "05030603020308040805080604010501060102010402050206020202002d0002"
     "0101001c00024001"
-) + bytes(963)
+) + bytes(962)
 LONG_CLIENT_ENCRYPTED_PACKET = binascii.unhexlify(
-    "c2ff000012508394c8f03e51570800449f9bd343fd65f354ebb400418b614f73"
-    "765009c0162d594777f9e6ddeb32fba3865cffd7e26e3724d4997cdde8df34f8"
-    "868772fed2412d43046f44dc7c6adf5ee10da456d56c892c8f69594594e8dcab"
-    "edb10d591130ca464588f2834eab931b10feb963c1947a05f57062692c242248"
-    "ad0133b31f6dcc585ba344ca5beb382fb619272e65dfccae59c08eb00b7d2a5b"
-    "bccd888582df1d1aee040aea76ab4dfdcae126791e71561b1f58312edb31c164"
-    "ff1341fd2820e2399946bad901e425dae58a9859ef1825e7d757a6291d9ba6ee"
-    "1a8c836dc0027cd705bd2bc67f56bad0024efaa3819cbb5d46cefdb7e0df3ad9"
-    "2b0689650e2b49ac29e6398bedc755541a3f3865bc4759bec74d721a28a0452c"
-    "1260189e8e92f844c91b27a00fc5ed6d14d8fceb5a848bea0a3208162c7a9578"
-    "2fcf9a045b20b76710a2565372f2541181030e4350e199e62fa4e2e0bba19ff6"
-    "6662ab8cc6815eeaa20b80d5f31c41e551f558d2c836a215ccff4e8afd2fec4b"
-    "fcb9ea9d051d12162f1b14842489b69d72a307d9144fced64fc4aa21ebd310f8"
-    "97cf00062e90dad5dbf04186622e6c1296d388176585fdb395358ecfec4d95db"
-    "4429f4473a76210866fd180eaeb60da433500c74c00aef24d77eae81755faa03"
-    "e71a8879937b32d31be2ba51d41b5d7a1fbb4d952b10dd2d6ec171a3187cf3f6"
-    "4d520afad796e4188bc32d153241c083f225b6e6b845ce9911bd3fe1eb4737b7"
-    "1c8d55e3962871b73657b1e2cce368c7400658d47cfd9290ed16cdc2a6e3e7dc"
-    "ea77fb5c6459303a32d58f62969d8f4670ce27f591c7a59cc3e7556eda4c58a3"
-    "2e9f53fd7f9d60a9c05cd6238c71e3c82d2efabd3b5177670b8d595151d7eb44"
-    "aa401fe3b5b87bdb88dffb2bfb6d1d0d8868a41ba96265ca7a68d06fc0b74bcc"
-    "ac55b038f8362b84d47f52744323d08b46bfec8c421f991e1394938a546a7482"
-    "a17c72be109ea4b0c71abc7d9c0ac0960327754e1043f18a32b9fb402fc33fdc"
-    "b6a0b4fdbbddbdf0d85779879e98ef211d104a5271f22823f16942cfa8ace68d"
-    "0c9e5b52297da9702d8f1de24bcd06284ac8aa1068fa21a82abbca7e7454b848"
-    "d7de8c3d43560541a362ff4f6be06c0115e3a733bff44417da11ae668857bba2"
-    "c53ba17db8c100f1b5c7c9ea960d3f3d3b9e77c16c31a222b498a7384e286b9b"
-    "7c45167d5703de715f9b06708403562dcff77fdf2793f94e294888cebe8da4ee"
-    "88a53e38f2430addc161e8b2e2f2d40541d10cda9a7aa518ac14d0195d8c2012"
-    "0b4f1d47d6d0909e69c4a0e641b83c1ad4fff85af4751035bc5698b6141ecc3f"
-    "bffcf2f55036880071ba1189274007967f64468172854d140d229320d689f576"
-    "60f6c445e629d15ff2dcdff4b71a41ec0c24bd2fd8f5ad13b2c3688e0fdb8dbc"
-    "ce42e6cf49cf60d022ccd5b19b4fd5d98dc10d9ce3a626851b1fdd23e1fa3a96"
-    "1f9b0333ab8d632e48c944b82bdd9e800fa2b2b9e31e96aee54b40edaf6b79ec"
-    "211fdc95d95ef552aa532583d76a539e988e416a0a10df2550cdeacafc3d61b0"
-    "b0a79337960a0be8cf6169e4d55fa6e7a9c2e8efabab3da008f5bcc38c1bbabd"
-    "b6c10368723da0ae83c4b1819ff54946e7806458d80d7be2c867d46fe1f029c5"
-    "8625313cf481f9541345f17eb544901f"
+    "c2ff000017088394c8f03e5157080000449e9bd343fd65f354ebb400418b614f"
+    "73765009c0162d594777f9e6ddeb32fba3865cffd7e26e3724d4997cdde8df34"
+    "f8868772fed2412d43046f44dc7c6adf5ee10da456d56c892c8f69594594e8dc"
+    "abedb10d591130ca464588f2834eab931b10feb963c1947a05f57062692c2422"
+    "48ad0133b31f6dcc585ba344ca5beb382fb619272e65dfccae59c08eb00b7d2a"
+    "5bbccd888582df1d1aee040aea76ab4dfdcae126791e71561b1f58312edb31c1"
+    "64ff1341fd2820e2399946bad901e425dae58a9859ef1825e7d757a6291d9ba6"
+    "ee1a8c836dc0027cd705bd2bc67f56bad0024efaa3819cbb5d46cefdb7e0df3a"
+    "d92b0689650e2b49ac29e6398bedc755541a3f3865bc4759bec74d721a28a045"
+    "2c1260189e8e92f844c91b27a00fc5ed6d14d8fceb5a848bea0a3208162c7a95"
+    "782fcf9a045b20b76710a2565372f2541181030e4350e199e62fa4e2e0bba19f"
+    "f66662ab8cc6815eeaa20b80d5f31c41e551f558d2c836a215ccff4e8afd2fec"
+    "4bfcb9ea9d051d12162f1b14842489b69d72a307d9144fced64fc4aa21ebd310"
+    "f897cf00062e90dad5dbf04186622e6c1296d388176585fdb395358ecfec4d95"
+    "db4429f4473a76210866fd180eaeb60da433500c74c00aef24d77eae81755faa"
+    "03e71a8879937b32d31be2ba51d41b5d7a1fbb4d952b10dd2d6ec171a3187cf3"
+    "f64d520afad796e4188bc32d153241c083f225b6e6b845ce9911bd3fe1eb4737"
+    "b71c8d55e3962871b73657b1e2cce368c7400658d47cfd9290ed16cdc2a6e3e7"
+    "dcea77fb5c6459303a32d58f62969d8f4670ce27f591c7a59cc3e7556eda4c58"
+    "a32e9f53fd7f9d60a9c05cd6238c71e3c82d2efabd3b5177670b8d595151d7eb"
+    "44aa401fe3b5b87bdb88dffb2bfb6d1d0d8868a41ba96265ca7a68d06fc0b74b"
+    "ccac55b038f8362b84d47f52744323d08b46bfec8c421f991e1394938a546a74"
+    "82a17c72be109ea4b0c71abc7d9c0ac0960327754e1043f18a32b9fb402fc33f"
+    "dcb6a0b4fdbbddbdf0d85779879e98ef211d104a5271f22823f16942cfa8ace6"
+    "8d0c9e5b52297da9702d8f1de24bcd06284ac8aa1068fa21a82abbca7e7454b8"
+    "48d7de8c3d43560541a362ff4f6be06c0115e3a733bff44417da11ae668857bb"
+    "a2c53ba17db8c100f1b5c7c9ea960d3f3d3b9e77c16c31a222b498a7384e286b"
+    "9b7c45167d5703de715f9b06708403562dcff77fdf2793f94e294888cebe8da4"
+    "ee88a53e38f2430addc161e8b2e2f2d40541d10cda9a7aa518ac14d0195d8c20"
+    "120b4f1d47d6d0909e69c4a0e641b83c1ad4fff85af4751035bc5698b6141ecc"
+    "3fbffcf2f55036880071ba1189274007967f64468172854d140d229320d689f5"
+    "7660f6c445e629d15ff2dcdff4b71a41ec0c24bd2fd8f5ad13b2c3688e0fdb8d"
+    "bcce42e6cf49cf60d022ccd5b19b4fd5d98dc10d9ce3a626851b1fdd23e1fa3a"
+    "961f9b0333ab8d632e48c944b82bdd9e800fa2b2b9e31e96aee54b40edaf6b79"
+    "ec211fdc95d95ef552aa532583d76a539e988e416a0a10df2550cdeacafc3d61"
+    "b0b0a79337960a0be8cf6169e4d55fa6e7a9c2e8efabab3da008f5bcc38c1bba"
+    "bdb6c10368723da0ae83c4b1819ff54946e7806458d80d7be2c867d46fe1f029"
+    "d9e8213693c88a498d54d924ebd38e0f"
 )
 
 LONG_SERVER_PACKET_NUMBER = 1
-LONG_SERVER_PLAIN_HEADER = binascii.unhexlify("c1ff00001205f067a5502a4262b50040740001")
+LONG_SERVER_PLAIN_HEADER = binascii.unhexlify(
+    "c1ff0000170008f067a5502a4262b50040740001"
+)
 LONG_SERVER_PLAIN_PAYLOAD = binascii.unhexlify(
     "0d0000000018410a020000560303eefce7f7b37ba1d1632e96677825ddf73988"
     "cfc79825df566dc5430b9a045a1200130100002e00330024001d00209d3c940d"
@@ -85,11 +87,11 @@ LONG_SERVER_PLAIN_PAYLOAD = binascii.unhexlify(
     "0304"
 )
 LONG_SERVER_ENCRYPTED_PACKET = binascii.unhexlify(
-    "caff00001205f067a5502a4262b5004074d74b7e486176fa3b713f272a9bf03e"
-    "e28d3c8addb4e805b3a110b663122a75eee93c9177ac6b7a6b548e15a7b8f884"
-    "65e9eab253a760779b2e6a2c574882b48d3a3eed696e50d04d5ec59af85261e4"
-    "cdbe264bd65f2b076760c69beef23aa714c9a174d6feeaf8c677cafb7486a838"
-    "61b0dd273a"
+    "caff0000170008f067a5502a4262b5004074d74b7e486176fa3b713f272a9bf0"
+    "3ee28d3c8addb4e805b3a110b663122a75eee93c9177ac6b7a6b548e15a7b8f8"
+    "8465e9eab253a760779b2e6a2c574882b48d3a3eed696e50d04d5ec59af85261"
+    "e4cdbe264bd65f2b076760c69beef23aa714c9a174d6d4a8853548bf96b19c54"
+    "6fffe1ab616b"
 )
 
 SHORT_SERVER_PACKET_NUMBER = 3
@@ -146,12 +148,12 @@ class CryptoTest(TestCase):
         pair.recv.setup(
             CipherSuite.CHACHA20_POLY1305_SHA256,
             binascii.unhexlify(
-                "fcc211ac3ff1f3fe1b096a41e701e30033cbd899494ebabbbc009ee2626d987e"
+                "b42772df33c9719a32820d302aa664d080d7f5ea7a71a330f87864cb289ae8c0"
             ),
         )
 
         plain_header, plain_payload, packet_number = pair.decrypt_packet(
-            CHACHA20_CLIENT_ENCRYPTED_PACKET, 34, 0
+            CHACHA20_CLIENT_ENCRYPTED_PACKET, 25, 0
         )
         self.assertEqual(plain_header, CHACHA20_CLIENT_PLAIN_HEADER)
         self.assertEqual(plain_payload, CHACHA20_CLIENT_PLAIN_PAYLOAD)
@@ -161,7 +163,7 @@ class CryptoTest(TestCase):
         pair = self.create_crypto(is_client=False)
 
         plain_header, plain_payload, packet_number = pair.decrypt_packet(
-            LONG_CLIENT_ENCRYPTED_PACKET, 17, 0
+            LONG_CLIENT_ENCRYPTED_PACKET, 18, 0
         )
         self.assertEqual(plain_header, LONG_CLIENT_PLAIN_HEADER)
         self.assertEqual(plain_payload, LONG_CLIENT_PLAIN_PAYLOAD)
@@ -171,7 +173,7 @@ class CryptoTest(TestCase):
         pair = self.create_crypto(is_client=True)
 
         plain_header, plain_payload, packet_number = pair.decrypt_packet(
-            LONG_SERVER_ENCRYPTED_PACKET, 17, 0
+            LONG_SERVER_ENCRYPTED_PACKET, 18, 0
         )
         self.assertEqual(plain_header, LONG_SERVER_PLAIN_HEADER)
         self.assertEqual(plain_payload, LONG_SERVER_PLAIN_PAYLOAD)
@@ -180,7 +182,7 @@ class CryptoTest(TestCase):
     def test_decrypt_no_key(self):
         pair = CryptoPair()
         with self.assertRaises(CryptoError):
-            pair.decrypt_packet(LONG_SERVER_ENCRYPTED_PACKET, 17, 0)
+            pair.decrypt_packet(LONG_SERVER_ENCRYPTED_PACKET, 18, 0)
 
     def test_decrypt_short_server(self):
         pair = CryptoPair()
@@ -204,7 +206,7 @@ class CryptoTest(TestCase):
         pair.send.setup(
             CipherSuite.CHACHA20_POLY1305_SHA256,
             binascii.unhexlify(
-                "fcc211ac3ff1f3fe1b096a41e701e30033cbd899494ebabbbc009ee2626d987e"
+                "b42772df33c9719a32820d302aa664d080d7f5ea7a71a330f87864cb289ae8c0"
             ),
         )
 
