49c65201f7cd1c1a0a23e91e57afc24dba8dfba3
Shuyi Wang <shuyi.wang@snapchat.com>
2019-11-06 22:21:49 -0800

    finished

--- examples/tmp/popen_read_std_as_pipe.py
+++ examples/tmp/popen_read_std_as_pipe.py
@@ -1,19 +1,41 @@
 import subprocess
+import asyncio
+from threading import Thread
+
+from bitstring import BitStream
 
 
-def run():
-    cmd = ["/anaconda3/envs/py37/bin/python3",
-           "/Users/shuyi.wang/Documents/others/aiortc/examples/tmp/stdout_same_line.py"]
+buffer = BitStream()
+
+
+def buffered_read():
+    global buffer
+    # cmd = ["/anaconda3/envs/py37/bin/python3",
+    #        "/Users/shuyi.wang/Documents/others/aiortc/examples/tmp/stdout_same_line.py"]
+    cmd = ["/home/sdjksdafji/anaconda3/envs/py37/bin/python3",
+           "/home/sdjksdafji/Documents/others/aiortc/examples/tmp/stdout_same_line.py"]
     proc = subprocess.Popen(
         cmd,
         stdout=subprocess.PIPE,
         stdin=subprocess.PIPE)
 
     while True:
-        print("start reading")
         data = proc.stdout.read(1)
-        print("finished reading with length: {0}".format(len(data)))
-        print(data)
+        buffer.append(data)
 
 
-run()
+async def run():
+    global buffer
+    buffered_read_thread = Thread(target=buffered_read)
+    buffered_read_thread.start()
+    while True:
+        print("Reading")
+        old_buffer = buffer
+        buffer = BitStream()
+        data = old_buffer.tobytes()
+        print("read data length: {0}, data: {1}".format(len(data), data))
+        print("Sleeping")
+        await asyncio.sleep(1.0)
+
+
+asyncio.run(run())
