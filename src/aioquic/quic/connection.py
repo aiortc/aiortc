656204deb0f5488a42f17628c5053deb8dfc57a4
Jeremy Lainé <jeremy.laine@m4x.org>
2019-11-17 23:52:57 +0100

    [connection] use dict for frame handlers
    This allows us to support future frame types which do not form a
    contiguous range, for example DATAGRAM frames.

--- src/aioquic/quic/connection.py
+++ src/aioquic/quic/connection.py
@@ -297,38 +297,38 @@ class QuicConnection:
         self._session_ticket_handler = session_ticket_handler
 
         # frame handlers
-        self.__frame_handlers = [
-            (self._handle_padding_frame, EPOCHS("IZHO")),
-            (self._handle_ping_frame, EPOCHS("IZHO")),
-            (self._handle_ack_frame, EPOCHS("IHO")),
-            (self._handle_ack_frame, EPOCHS("IHO")),
-            (self._handle_reset_stream_frame, EPOCHS("ZO")),
-            (self._handle_stop_sending_frame, EPOCHS("ZO")),
-            (self._handle_crypto_frame, EPOCHS("IHO")),
-            (self._handle_new_token_frame, EPOCHS("O")),
-            (self._handle_stream_frame, EPOCHS("ZO")),
-            (self._handle_stream_frame, EPOCHS("ZO")),
-            (self._handle_stream_frame, EPOCHS("ZO")),
-            (self._handle_stream_frame, EPOCHS("ZO")),
-            (self._handle_stream_frame, EPOCHS("ZO")),
-            (self._handle_stream_frame, EPOCHS("ZO")),
-            (self._handle_stream_frame, EPOCHS("ZO")),
-            (self._handle_stream_frame, EPOCHS("ZO")),
-            (self._handle_max_data_frame, EPOCHS("ZO")),
-            (self._handle_max_stream_data_frame, EPOCHS("ZO")),
-            (self._handle_max_streams_bidi_frame, EPOCHS("ZO")),
-            (self._handle_max_streams_uni_frame, EPOCHS("ZO")),
-            (self._handle_data_blocked_frame, EPOCHS("ZO")),
-            (self._handle_stream_data_blocked_frame, EPOCHS("ZO")),
-            (self._handle_streams_blocked_frame, EPOCHS("ZO")),
-            (self._handle_streams_blocked_frame, EPOCHS("ZO")),
-            (self._handle_new_connection_id_frame, EPOCHS("ZO")),
-            (self._handle_retire_connection_id_frame, EPOCHS("O")),
-            (self._handle_path_challenge_frame, EPOCHS("ZO")),
-            (self._handle_path_response_frame, EPOCHS("O")),
-            (self._handle_connection_close_frame, EPOCHS("IZHO")),
-            (self._handle_connection_close_frame, EPOCHS("ZO")),
-        ]
+        self.__frame_handlers = {
+            0x00: (self._handle_padding_frame, EPOCHS("IZHO")),
+            0x01: (self._handle_ping_frame, EPOCHS("IZHO")),
+            0x02: (self._handle_ack_frame, EPOCHS("IHO")),
+            0x03: (self._handle_ack_frame, EPOCHS("IHO")),
+            0x04: (self._handle_reset_stream_frame, EPOCHS("ZO")),
+            0x05: (self._handle_stop_sending_frame, EPOCHS("ZO")),
+            0x06: (self._handle_crypto_frame, EPOCHS("IHO")),
+            0x07: (self._handle_new_token_frame, EPOCHS("O")),
+            0x08: (self._handle_stream_frame, EPOCHS("ZO")),
+            0x09: (self._handle_stream_frame, EPOCHS("ZO")),
+            0x0A: (self._handle_stream_frame, EPOCHS("ZO")),
+            0x0B: (self._handle_stream_frame, EPOCHS("ZO")),
+            0x0C: (self._handle_stream_frame, EPOCHS("ZO")),
+            0x0D: (self._handle_stream_frame, EPOCHS("ZO")),
+            0x0E: (self._handle_stream_frame, EPOCHS("ZO")),
+            0x0F: (self._handle_stream_frame, EPOCHS("ZO")),
+            0x10: (self._handle_max_data_frame, EPOCHS("ZO")),
+            0x11: (self._handle_max_stream_data_frame, EPOCHS("ZO")),
+            0x12: (self._handle_max_streams_bidi_frame, EPOCHS("ZO")),
+            0x13: (self._handle_max_streams_uni_frame, EPOCHS("ZO")),
+            0x14: (self._handle_data_blocked_frame, EPOCHS("ZO")),
+            0x15: (self._handle_stream_data_blocked_frame, EPOCHS("ZO")),
+            0x16: (self._handle_streams_blocked_frame, EPOCHS("ZO")),
+            0x17: (self._handle_streams_blocked_frame, EPOCHS("ZO")),
+            0x18: (self._handle_new_connection_id_frame, EPOCHS("ZO")),
+            0x19: (self._handle_retire_connection_id_frame, EPOCHS("O")),
+            0x1A: (self._handle_path_challenge_frame, EPOCHS("ZO")),
+            0x1B: (self._handle_path_response_frame, EPOCHS("O")),
+            0x1C: (self._handle_connection_close_frame, EPOCHS("IZHO")),
+            0x1D: (self._handle_connection_close_frame, EPOCHS("ZO")),
+        }
 
     @property
     def configuration(self) -> QuicConfiguration:
@@ -1731,7 +1731,7 @@ class QuicConnection:
             # check frame type is known
             try:
                 frame_handler, frame_epochs = self.__frame_handlers[frame_type]
-            except IndexError:
+            except KeyError:
                 raise QuicConnectionError(
                     error_code=QuicErrorCode.PROTOCOL_VIOLATION,
                     frame_type=frame_type,
