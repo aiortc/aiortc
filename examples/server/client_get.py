78c4ea2345b7c9c7368e30835c79f1f3735dc4fe
adblock-filters <bartorbard@gmail.com>
2019-12-29 01:41:02 +0100

    fix flask img send

--- examples/server/client_get.py
+++ examples/server/client_get.py
@@ -6,8 +6,8 @@ import socket
 with open("img.jpg", "rb") as img_file:
     img_encoded = base64.b64encode(img_file.read())
 # print(my_string)
-hostname = socket.gethostname()    
-ip_addr = socket.gethostbyname(hostname)  
-r = requests.post("http://127.0.0.1:5000/filter", data={'image': img_encoded, 'ip': ip_addr })
+
+# r = requests.post("http://bbbrtk.site:5000/filter", data={'image': img_encoded})
+r = requests.post("http://127.0.0.1:5000/filter", data={'image': img_encoded})
 
 print(r.text) # displays the result body.
--- examples/server/get.py
+++ examples/server/get.py
@@ -4,10 +4,10 @@ import base64
 app = Flask(__name__)
 @app.route('/filter', methods=['POST'])
 def result():
-    ip = request.form['ip']
+    ip = request.environ['REMOTE_ADDR']
     img = request.form['image'].encode('utf-8')
     img_name = f"{ip.replace('.','-')}.jpg"
     
     with open(img_name, "wb") as fh:
         fh.write(base64.decodestring(img))
-    return 'received' # response to request
+    return f"received image {img_name}" # response to request
