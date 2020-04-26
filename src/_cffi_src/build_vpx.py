210e5e8af52f5ce54b77a5cd6808da210c11073d
Jeremy Lainé <jeremy.laine@m4x.org>
2020-04-27 00:17:17 +0200

    [package] produce binary wheels for Windows
    Use pre-built codecs on all platforms to speed up builds.

--- src/_cffi_src/build_vpx.py
+++ src/_cffi_src/build_vpx.py
@@ -1,4 +1,11 @@
+import sys
+
 from cffi import FFI
+
+if sys.platform == "win32":
+    libraries = ["vpxmt"]
+else:
+    libraries = ["vpx"]
 
 ffibuilder = FFI()
 
@@ -29,7 +36,7 @@ vpx_codec_err_t vpx_codec_enc_init(vpx_codec_ctx_t *ctx,
     return vpx_codec_enc_init_ver(ctx, iface, cfg, flags, VPX_ENCODER_ABI_VERSION);
 }
     """,
-    libraries=["vpx"],
+    libraries=libraries,
 )
 
 ffibuilder.cdef(
