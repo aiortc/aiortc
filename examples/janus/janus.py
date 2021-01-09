1f92b73b534fc950f1c95e85a9c12e6662f2616e
Jonathan <jonathanxu25@gmail.com>
2021-01-09 02:40:14 -0800

    fix Invalid element type (room should be a string)

--- examples/janus/janus.py
+++ examples/janus/janus.py
@@ -246,7 +246,7 @@ if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     try:
         loop.run_until_complete(
-            run(player=player, recorder=recorder, room=args.room, session=session)
+            run(player=player, recorder=recorder, room=str(args.room), session=session)
         )
     except KeyboardInterrupt:
         pass
