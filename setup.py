c9a4ef60460bfdda9d5baf95a57e556bbb359dae
Jeremy Lainé <jeremy.laine@m4x.org>
2021-01-27 13:47:41 +0100

    [sctp] fix import warning for crc32c
    The crc32c package renamed the `crc32` function to `crc32c` as in
    release 2.1. It is also no longer necessary to set `CRC32C_SW_MODE`
    since release 2.0.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ install_requires = [
     "aioice>=0.7.0,<0.8.0",
     "av>=8.0.0,<9.0.0",
     "cffi>=1.0.0",
-    "crc32c",
+    "crc32c>=2.1",
     "cryptography>=2.2",
     'dataclasses; python_version < "3.7"',
     "pyee>=6.0.0",
