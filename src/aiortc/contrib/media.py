5be9ddbf9aee432c30bb12a4bb3ee0ad4fb3b644
Pantea Karimi Babaahmadi <pkarimib@mit.edu>
2022-04-10 19:37:35 +0000

    env var

--- src/aiortc/contrib/media.py
+++ src/aiortc/contrib/media.py
@@ -21,7 +21,7 @@ from first_order_model.fom_wrapper import FirstOrderModel
 
 # instantiate and warm up the model
 time_before_instantiation = time.perf_counter()
-config_path = '/data3/vibhaa/aiortc/nets_implementation/first_order_model/config/overview_exps_for_512_resolution.yaml'
+config_path = os.environ.get('CONFIG_PATH')
 checkpoint = os.environ.get('CHECKPOINT_PATH', 'None')
 model = FirstOrderModel(config_path, checkpoint)
 for i in range(100):
