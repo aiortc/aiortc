01f4c3a42c5fbded21ebb64475122f25e85b4f44
Jeremy Lainé <jeremy.laine@m4x.org>
2023-12-01 00:40:03 +0100

    Update readthedocs configuration file

--- setup.py
+++ setup.py
@@ -1,14 +1,8 @@
-import os
-
 import setuptools
 
-cffi_modules = [
-    "src/_cffi_src/build_opus.py:ffibuilder",
-    "src/_cffi_src/build_vpx.py:ffibuilder",
-]
-
-# Do not build cffi modules on readthedocs as we lack the codec development files.
-if os.environ.get("READTHEDOCS") == "True":
-    cffi_modules = []
-
-setuptools.setup(cffi_modules=cffi_modules)
+setuptools.setup(
+    cffi_modules=[
+        "src/_cffi_src/build_opus.py:ffibuilder",
+        "src/_cffi_src/build_vpx.py:ffibuilder",
+    ]
+)
