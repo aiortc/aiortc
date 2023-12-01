50f9c98fb639175e133c99020f92935af558a826
Jeremy Lainé <jeremy.laine@m4x.org>
2023-12-01 01:17:37 +0100

    Build wheels using Python limited API

--- setup.py
+++ setup.py
@@ -1,8 +1,21 @@
 import setuptools
+from wheel.bdist_wheel import bdist_wheel
+
+
+class bdist_wheel_abi3(bdist_wheel):
+    def get_tag(self):
+        python, abi, plat = super().get_tag()
+
+        if python.startswith("cp"):
+            return "cp38", "abi3", plat
+
+        return python, abi, plat
+
 
 setuptools.setup(
     cffi_modules=[
         "src/_cffi_src/build_opus.py:ffibuilder",
         "src/_cffi_src/build_vpx.py:ffibuilder",
-    ]
+    ],
+    cmdclass={"bdist_wheel": bdist_wheel_abi3},
 )
