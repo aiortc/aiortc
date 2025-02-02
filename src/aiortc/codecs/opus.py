19d2d89dbe20c6d7e2271d3cd3702ea0ef17b7fa
Dogan Kurt <dogan.kurt@dodobyte.com>
2025-02-03 02:11:01 +0300

    Update opus codec to use PyAV instead of libopus

--- src/aiortc/codecs/opus.py
+++ src/aiortc/codecs/opus.py
@@ -1,13 +1,13 @@
 import fractions
 from typing import List, Tuple
 
-from av import AudioFrame, AudioResampler
+from av import AudioFrame, CodecContext
+from av.audio.resampler import AudioResampler
 from av.frame import Frame
 from av.packet import Packet
 
 from ..jitterbuffer import JitterFrame
 from ..mediastreams import convert_timebase
-from ._opus import ffi, lib
 from .base import Decoder, Encoder
 
 CHANNELS = 2
@@ -19,44 +19,31 @@ TIME_BASE = fractions.Fraction(1, SAMPLE_RATE)
 
 class OpusDecoder(Decoder):
     def __init__(self) -> None:
-        error = ffi.new("int *")
-        self.decoder = lib.opus_decoder_create(SAMPLE_RATE, CHANNELS, error)
-        assert error[0] == lib.OPUS_OK
+        self.decoder = CodecContext.create("libopus", "r")
+        self.decoder.open()
 
     def __del__(self) -> None:
-        lib.opus_decoder_destroy(self.decoder)
+        self.decoder.close()
 
     def decode(self, encoded_frame: JitterFrame) -> List[Frame]:
-        frame = AudioFrame(format="s16", layout="stereo", samples=SAMPLES_PER_FRAME)
-        frame.pts = encoded_frame.timestamp
-        frame.sample_rate = SAMPLE_RATE
-        frame.time_base = TIME_BASE
-
-        length = lib.opus_decode(
-            self.decoder,
-            encoded_frame.data,
-            len(encoded_frame.data),
-            ffi.cast("int16_t *", frame.planes[0].buffer_ptr),
-            SAMPLES_PER_FRAME,
-            0,
-        )
-        assert length == SAMPLES_PER_FRAME
-        return [frame]
+        frames = self.decoder.decode(Packet(encoded_frame.data))
+        assert len(frames) == 1 and frames[0].samples == SAMPLES_PER_FRAME
+        frames[0].pts = encoded_frame.timestamp
+        frames[0].sample_rate = SAMPLE_RATE
+        frames[0].time_base = TIME_BASE
+        return frames
 
 
 class OpusEncoder(Encoder):
     def __init__(self) -> None:
-        error = ffi.new("int *")
-        self.encoder = lib.opus_encoder_create(
-            SAMPLE_RATE, CHANNELS, lib.OPUS_APPLICATION_VOIP, error
-        )
-        assert error[0] == lib.OPUS_OK
-
-        self.cdata = ffi.new(
-            "unsigned char []", SAMPLES_PER_FRAME * CHANNELS * SAMPLE_WIDTH
-        )
-        self.buffer = ffi.buffer(self.cdata)
-
+        codec = CodecContext.create("libopus", "w")
+        codec.format = "s16"
+        codec.layout = "stereo"
+        codec.sample_rate = SAMPLE_RATE
+        codec.bit_rate = 128000
+        codec.time_base = TIME_BASE
+        self.encoder = codec
+        self.encoder.open()
         # Create our own resampler to control the frame size.
         self.resampler = AudioResampler(
             format="s16",
@@ -66,7 +53,7 @@ class OpusEncoder(Encoder):
         )
 
     def __del__(self) -> None:
-        lib.opus_encoder_destroy(self.encoder)
+        self.encoder.close()
 
     def encode(
         self, frame: Frame, force_keyframe: bool = False
@@ -74,25 +61,14 @@ class OpusEncoder(Encoder):
         assert isinstance(frame, AudioFrame)
         assert frame.format.name == "s16"
         assert frame.layout.name in ["mono", "stereo"]
-
         # Send frame through resampler and encoder.
         payloads = []
         timestamp = None
         for frame in self.resampler.resample(frame):
-            data = bytes(frame.planes[0])
-            length = lib.opus_encode(
-                self.encoder,
-                ffi.cast("int16_t*", ffi.from_buffer(data)),
-                SAMPLES_PER_FRAME,
-                self.cdata,
-                len(self.cdata),
-            )
-            assert length > 0
-
-            payloads.append(self.buffer[0:length])
+            for packet in self.encoder.encode(frame):
+                payloads.append(bytes(packet))
             if timestamp is None:
                 timestamp = frame.pts
-
         return payloads, timestamp
 
     def pack(self, packet: Packet) -> Tuple[List[bytes], int]:
