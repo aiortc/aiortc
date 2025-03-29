aef9d56febdab9ccb252f08c209349f9e5ef77e6
Jeremy Lainé <jeremy.laine@m4x.org>
2025-03-29 22:55:01 +0100

    Don't touch PyAV's logging configuration
    Nowadays PyAV's default logging configuration no longer causes
    deadlocks. Furthermore it is much less noisy as it suppresses FFmpeg's
    log messages.

--- src/aiortc/__init__.py
+++ src/aiortc/__init__.py
@@ -1,8 +1,6 @@
 # ruff: noqa: F401
 import logging
 
-import av.logging
-
 from .exceptions import InvalidAccessError, InvalidStateError
 from .mediastreams import (
     AudioStreamTrack,
@@ -53,9 +51,6 @@ from .stats import (
 )
 
 __version__ = "1.11.0"
-
-# Disable PyAV's logging framework as it can lead to thread deadlocks.
-av.logging.restore_default_callback()
 
 # Set default logging handler to avoid "No handler found" warnings.
 logging.getLogger(__name__).addHandler(logging.NullHandler())
